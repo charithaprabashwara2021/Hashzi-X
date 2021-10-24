cec4f0a88edac4e8372c3e875f5c4e528460660d
charithaprabashwara2021 <92076942+charithaprabashwara2021@users.noreply.github.com>
2021-10-24 10:03:52 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,64 +1,69 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2020 Charith Production.
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+
+WhatsAsena - Yusuf Usta
 */
 
 const chalk = require('chalk');
 const { WAConnection, MessageType } = require('@adiwajshing/baileys');
 const fs = require('fs');
+
 async function whatsAsena() {
-  const conn = new WAConnection();
-  conn.logger.level = 'warn';
-  conn.version = [2, 2126, 14]
+	const conn = new WAConnection();
+	conn.logger.level = 'warn';
+	conn.version = [2, 2123, 8]
 
-  conn.on('connecting', async () => {
-    console.log(`${chalk.green.bold('Amazone Alexa')}${chalk.green.bold('Team')}
-${chalk.white.italic('amazone String code recipient')}
-${chalk.blue.bold('ℹ️  Connecting alexa... Please wait.')}`);
-  });
+	conn.on('connecting', async () => {
+		console.log(`${chalk.green.bold('CHARITHBOT')}${chalk.blue.bold('X')}
+${chalk.white.italic('Charithbot QR කේතය ')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+	});
 
-  conn.on('open', async () => {
-    console.log(
-      chalk.green.bold('Alexa QR Code: '),
-      'NEUTRO;;;' +
-      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-        'base64'
-      )
-    );
-    await conn.sendMessage(
-      conn.user.jid,
-      'NEUTRO;;;' +
-      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-        'base64'
-      ),
-      MessageType.text
-    );
-    if (conn.user.jid.startsWith('91')) {
-      await conn.sendMessage(
-        conn.user.jid,
-        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ Amazone Scanned✅️*\n*▪️Thanks For Choosing Amazone 💞*',
-        MessageType.text
-      );
-    } else {
-      await conn.sendMessage(
-        conn.user.jid,
-        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ Amazone Successfully Scanned✅️*\n*▪️Thanks For Choosing Amazone 💞*',
-        MessageType.text
-      );
-    }
-    console.log(
-      chalk.green.bold(
-        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
-      ),
-      chalk.green.bold(
-        'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
-      )
-    );
-    process.exit(0);
-  });
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('CHARITHBOT QR Code: '),
+			'CHARITH;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'CHARITH;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('90')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Lütfen Bu Kodu Kimseyle Paylaşma ' + conn.user.name + '* ⚠️',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"EĞER MESAJI KOPYALAMIYORSANIZ LÜTFEN WHATSAPP'I KONTROL EDİN. KENDİ NUMARANIZA QR KODU GÖNDERİLDİ!\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+			)
+		);
+		process.exit(0);
+	});
 
-  await conn.connect();
+	await conn.connect();
 }
 
 whatsAsena();
